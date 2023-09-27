892a83f8e280d9486da045464d14ced4e69e5f7d
bwhitehead0 <whitehead.brandon@gmail.com>
2023-09-27 18:04:12 -0500

    basic service working

--- main.go
+++ main.go
@@ -24,15 +24,47 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
-func simpleResponse(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /simpleResponse request\n")
-	io.WriteString(w, "This is the simple response.\n")
+func checkServiceSimple(service string) string {
+	cmd := exec.Command("systemctl", "check", service)
+
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			fmt.Printf("systemctl finished with non-zero: %v\n", exitErr)
+		} else {
+			fmt.Printf("failed to run systemctl: %v", err)
+			os.Exit(1)
+		}
+	}
+	fmt.Printf("%s\n", string(out))
+	return string(out)
+}
+
+// func simpleResponse(w http.ResponseWriter, r *http.Request, status string) {
+// 	fmt.Printf("got /status request\n")
+// 	io.WriteString(w, "This is the simple response.\n")
+// 	io.WriteString(w, status)
+// }
+
+func response(output string) http.HandlerFunc {
+	// using a wrapped handler https://go-cloud-native.com/golang/pass-arguments-to-http-handlers-in-go
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, output)
+	}
 }
 
 func main() {
-	http.HandleFunc("/simpleResponse", simpleResponse)
+	var serviceName string = "plexmediaserver"
+	var serviceStatus string
+
+	serviceStatus = checkServiceSimple(serviceName)
+	http.HandleFunc("/status", response(serviceStatus))
+
+	// response(serviceStatus)
 
 	err := http.ListenAndServe(":33131", nil)
 
